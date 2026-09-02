3699fdd39f7045989ac2a9f192692d371a7c98dc
zheleznov-sema <zheleznov.s@bk.ru>
2026-09-02 23:58:13 +0300

    fix v3

--- src/test/java/ru/netology/page/VerificationPage.java
+++ src/test/java/ru/netology/page/VerificationPage.java
@@ -1,6 +1,7 @@
 package ru.netology.page;
 
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.WebDriverRunner;
 
 import static com.codeborne.selenide.Selenide.$;
 
@@ -9,12 +10,13 @@ public class VerificationPage {
     private final SelenideElement codeField = $("[data-test-id='code'] input");
     private final SelenideElement verifyButton  = $("[data-test-id='action-verify']");
 
-    public DashboardPage validVerify(String verificationCode){
+    public DashboardPage validVerify(String verificationCode) {
         codeField.setValue(verificationCode);
         verifyButton.click();
 
-        return new DashboardPage();
+        System.out.println("урла после verify: " + WebDriverRunner.url());
 
+        return new DashboardPage();
     }
 
 }
